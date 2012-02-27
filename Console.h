5226775b49fd3877ac33b4aecd220a44005eb014
Kehnin Dyer <kehnin@gmail.com>
2012-02-27 01:37:09 -0800

    Added constant COORD origin and 1 and 0 rectangles in the nitrocorp::console namespace
    minor formating fixes

--- Console.h
+++ Console.h
@@ -8,6 +8,9 @@
 
 namespace nitrocorp { namespace console {
 
+COORD const ORIGIN = {0,0};
+SMALL_RECT const ZERO_RECT = {0,0,0,0};
+SMALL_RECT const ONE_RECT = {0,0,1,1};
 
 template <typename charT>
 class console
@@ -43,20 +46,19 @@ private:
 		//method 1
 		CHAR_INFO * const buffer = new CHAR_INFO [BufferSize.X * BufferSize.Y];
 		COORD size = BufferSize;
-		COORD pos = {0,0};
 		SMALL_RECT rec = {0,0,BufferSize.X,BufferSize.Y};
 		ReadConsoleOutput(
 						ConsoleState->stout,
 						buffer,
 						size,
-						pos,
+						ORIGIN,
 						&rec
 					);
 		WriteConsoleOutput(
 						ConsoleState->out_buffer,
 						buffer,
 						size,
-						pos,
+						ORIGIN,
 						&rec
 					);
 		delete[] buffer;
@@ -103,108 +105,105 @@ public:
 
 //Console Modifiers
 	void SetConsoleSize(COORD size)
-{
-	SetConsoleSize(size.X, size.Y);
-}
+	{
+		SetConsoleSize(size.X, size.Y);
+	}
 	void SetConsoleSize(SHORT x, SHORT y)
-{
-	SMALL_RECT WindowRect;
-	COORD ScreenCoord;
+	{
+		SMALL_RECT WindowRect;
+		COORD ScreenCoord;
 
-	//if the size is the same.... well we're done.
-	if(x==BufferSize.X && y==BufferSize.Y)
-		return;
+		//if the size is the same.... well we're done.
+		if(x==BufferSize.X && y==BufferSize.Y)
+			return;
 
-	ScreenCoord = GetLargestConsoleWindowSize(ConsoleState->stout);
+		ScreenCoord = GetLargestConsoleWindowSize(ConsoleState->stout);
 		
-	//we want to make the screen no larger than the max size
-	//TODO: check for minimum
-	x = (SHORT) (min(x, ScreenCoord.X));
-	y = (SHORT) (min(y, ScreenCoord.Y));
-	COORD fontSize = GetFontSize();
-	x = (SHORT) (max(x, GetSystemMetrics(SM_CXMIN) / fontSize.X));
-	y = (SHORT) (max(y, GetSystemMetrics(SM_CYMIN) / fontSize.Y));
-	//make the rectangle 1 smaller than what we want so we can force the
-	//scroll bars away
-	WindowRect.Right = x - 1; 
-	WindowRect.Bottom = y - 1;
-	WindowRect.Left = WindowRect.Top = (SHORT) 0;
-	if(SetConsoleScreenBufferSize(ConsoleState->stout, ScreenCoord) == 0)
-		throw (GetLastError());
-	//do the same for second buffer
-	if(SetConsoleScreenBufferSize(ConsoleState->out_buffer, ScreenCoord) == 0)
-		throw (GetLastError());
+		//we want to make the screen no larger than the max size
+		//TODO: check for minimum
+		x = (SHORT) (min(x, ScreenCoord.X));
+		y = (SHORT) (min(y, ScreenCoord.Y));
+		COORD fontSize = GetFontSize();
+		x = (SHORT) (max(x, GetSystemMetrics(SM_CXMIN) / fontSize.X));
+		y = (SHORT) (max(y, GetSystemMetrics(SM_CYMIN) / fontSize.Y));
+		//make the rectangle 1 smaller than what we want so we can force the
+		//scroll bars away
+		WindowRect.Right = x - 1; 
+		WindowRect.Bottom = y - 1;
+		WindowRect.Left = WindowRect.Top = (SHORT) 0;
+		if(SetConsoleScreenBufferSize(ConsoleState->stout, ScreenCoord) == 0)
+			throw (GetLastError());
+		//do the same for second buffer
+		if(SetConsoleScreenBufferSize(ConsoleState->out_buffer, ScreenCoord) == 0)
+			throw (GetLastError());
 
-	if(SetConsoleWindowInfo(ConsoleState->stout, TRUE, &WindowRect) == 0)
-		throw (GetLastError());
-	if(SetConsoleWindowInfo(ConsoleState->out_buffer, TRUE, &WindowRect) == 0)
-		throw (GetLastError());
+		if(SetConsoleWindowInfo(ConsoleState->stout, TRUE, &WindowRect) == 0)
+			throw (GetLastError());
+		if(SetConsoleWindowInfo(ConsoleState->out_buffer, TRUE, &WindowRect) == 0)
+			throw (GetLastError());
 
-	//and now that the info thinks it is smaller than we want increase the
-	//buffer to be the size of the console
-	ScreenCoord.X = x;
-	ScreenCoord.Y = y;
-	if(SetConsoleScreenBufferSize(ConsoleState->stout, ScreenCoord) == 0)
-		throw(GetLastError());
-	if(SetConsoleScreenBufferSize(ConsoleState->out_buffer, ScreenCoord) == 0)
-		throw(GetLastError());
+		//and now that the info thinks it is smaller than we want increase the
+		//buffer to be the size of the console
+		ScreenCoord.X = x;
+		ScreenCoord.Y = y;
+		if(SetConsoleScreenBufferSize(ConsoleState->stout, ScreenCoord) == 0)
+			throw(GetLastError());
+		if(SetConsoleScreenBufferSize(ConsoleState->out_buffer, ScreenCoord) == 0)
+			throw(GetLastError());
 
-	BufferSize.X = x;//make the size variables the size of the buffers
-	BufferSize.Y = y;
-
-}
+		BufferSize.X = x;//make the size variables the size of the buffers
+		BufferSize.Y = y;
+	
+	}
 
 	void HideCursor()
-{
-	//hide cursor
-	CONSOLE_CURSOR_INFO info;
-	GetConsoleCursorInfo(ConsoleState->stout,&info);
-	info.bVisible=false;
-	SetConsoleCursorInfo(ConsoleState->stout,&info);
-	SetConsoleCursorInfo(ConsoleState->out_buffer,&info);
-}
+	{
+		//hide cursor
+		CONSOLE_CURSOR_INFO info;
+		GetConsoleCursorInfo(ConsoleState->stout,&info);
+		info.bVisible=false;
+		SetConsoleCursorInfo(ConsoleState->stout,&info);
+		SetConsoleCursorInfo(ConsoleState->out_buffer,&info);
+	}
 	void ShowCursor()
-{
-	//hide cursor
-	CONSOLE_CURSOR_INFO info;
-	GetConsoleCursorInfo(ConsoleState->stout,&info);
-	info.bVisible=true;
-	SetConsoleCursorInfo(ConsoleState->stout,&info);
-	SetConsoleCursorInfo(ConsoleState->out_buffer,&info);
-}
+	{
+		//hide cursor
+		CONSOLE_CURSOR_INFO info;
+		GetConsoleCursorInfo(ConsoleState->stout,&info);
+		info.bVisible=true;
+		SetConsoleCursorInfo(ConsoleState->stout,&info);
+		SetConsoleCursorInfo(ConsoleState->out_buffer,&info);
+	}
 	void DisableWraping()
-{
-	DWORD mode;
-	GetConsoleMode(ConsoleState->stout, &mode);
-	mode &= ~ENABLE_WRAP_AT_EOL_OUTPUT;
-	SetConsoleMode(ConsoleState->stout, mode);
-	SetConsoleMode(ConsoleState->out_buffer, mode);
-}
+	{
+		DWORD mode;
+		GetConsoleMode(ConsoleState->stout, &mode);
+		mode &= ~ENABLE_WRAP_AT_EOL_OUTPUT;
+		SetConsoleMode(ConsoleState->stout, mode);
+		SetConsoleMode(ConsoleState->out_buffer, mode);
+	}
 	void EnableWraping()
-{
-	DWORD mode;
-	GetConsoleMode(ConsoleState->stout, &mode);
-	mode |= ENABLE_WRAP_AT_EOL_OUTPUT;
-	SetConsoleMode(ConsoleState->stout, mode);
-	SetConsoleMode(ConsoleState->out_buffer, mode);
-}
+	{
+		DWORD mode;
+		GetConsoleMode(ConsoleState->stout, &mode);
+		mode |= ENABLE_WRAP_AT_EOL_OUTPUT;
+		SetConsoleMode(ConsoleState->stout, mode);
+		SetConsoleMode(ConsoleState->out_buffer, mode);
+	}
 
 	void SetFontSize(SHORT x, SHORT y)
-{
-	CONSOLE_FONT_INFOEX fn;
-	fn.cbSize = sizeof( CONSOLE_FONT_INFOEX );
-	GetCurrentConsoleFontEx(ConsoleState->stout, false, &fn);
-	fn.dwFontSize.X = x;
-	fn.dwFontSize.Y = y;
-	SetCurrentConsoleFontEx(ConsoleState->stout, false, &fn);
-	SetCurrentConsoleFontEx(ConsoleState->out_buffer, false, &fn)
-}
+	{
+		CONSOLE_FONT_INFOEX fn;
+		fn.cbSize = sizeof( CONSOLE_FONT_INFOEX );
+		GetCurrentConsoleFontEx(ConsoleState->stout, false, &fn);
+		fn.dwFontSize.X = x;
+		fn.dwFontSize.Y = y;
+		SetCurrentConsoleFontEx(ConsoleState->stout, false, &fn);
+		SetCurrentConsoleFontEx(ConsoleState->out_buffer, false, &fn)
+	}
 	
 
-	void Draw()
-	{
-		SwapDisplayBuffers();
-	}
+
 //AUX Functions
 
 
@@ -221,16 +220,18 @@ public:
 
 };
 
+/*
 bool operator== (CHAR_INFO& a, CHAR_INFO& b)
 {
 	return true;
 }
-
+*/
 template<typename charT> console<charT> console<charT>::InnerConsole;
 typedef console<char> console_;
 typedef console<wchar_t> console_w;
 typedef console<TCHAR> console_T;
 
 }}
+
 #endif
 
