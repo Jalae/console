a1ee6915c49d10d94d0f4b20f543691feab01795
Kehnin Dyer <kehnin@gmail.com>
2012-04-09 21:36:36 -0700

    adding object file

---
+++ Console_Object.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+*Author:		Kehnin Dyer
+*File name:		Console_Object.h
+*Date Created:	2012/04/09
+*Modifed:		
+*					:see git commits for more details
+******************************************************************************/
+
+
+
+#include "Console.h"
+
+#ifndef _NC_CONSOLE_OBJECT__
+#define _NC_CONSOLE_OBJECT__
+
+namespace nitrocorp{ namespace console {
+
+
+}}
+#endif
